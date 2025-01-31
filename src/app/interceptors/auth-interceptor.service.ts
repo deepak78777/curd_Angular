46d3714e33fa7bb5c217f592fab58fd8f97d7f3c
deepak78777 <deepak516421@gmail.com>
2025-01-31 10:15:18 +0530

    records in table

---
+++ src/app/interceptors/auth-interceptor.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { AuthServiceService } from '../services/auth-service.service';
+
+@Injectable()
+export class AuthInterceptorService implements HttpInterceptor {
+  constructor (private token:AuthServiceService){}
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const token = this.token.getToken// Get token from localStorage
+
+    if (token) {
+      // Clone request and add Authorization header
+      const clonedReq = req.clone({
+        setHeaders: {
+          Authorization: `Bearer ${token}`
+        }
+      });
+      return next.handle(clonedReq);
+    }
+
+    return next.handle(req);
+  }
+}
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -6,4 +6,9 @@ import { Injectable } from '@angular/core';
 export class ApiService {
 
   constructor() { }
+  public MainUrl = 'http://localhost:8000/';
+
+  public records=`${this.MainUrl}/api/records/`
+  public create=`${this.MainUrl}/api/create/`
+
 }
